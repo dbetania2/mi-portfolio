2bbe702f98de26b9b65036a73710ad7b9474872a
dbetania2 <109638715+dbetania2@users.noreply.github.com>
2026-03-30 10:52:21 -0300

    style(global): actualización estilos globales y page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import { supabase } from '@/lib/supabase'
 import Hero from '@/components/hero/Hero'
 import About from '@/components/about/About'
 import Skills from '@/components/skills/Skills'
 import Projects from '@/components/projects/Projects'
 import Contact from '@/components/contact/Contact'
 import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
-export default function Home() {
+
+
+export default async function Home() {
+
   return (
     <main>
       <Navbar />
@@ -14,6 +19,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   )
 }
